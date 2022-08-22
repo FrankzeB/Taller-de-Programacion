b90cc487fbe58ff076ef6ccb5a1de434c5d6fe9e
FrankzeB <franezequielbravo05@gmail.com>
2022-08-22 17:10:09 -0300

    Cambios

--- Clase III (22-08-22) - Actividad Nº2/main.js
+++ Clase III (22-08-22) - Actividad Nº2/main.js
@@ -1,13 +1,19 @@
 import Pitagoras from "./pitagoras.js";
 
 function ejecutar() {
+  //creamos la instancia de la clase
   let pitagoras = new Pitagoras();
 
+  /*A través de la instancia accedemos a los atributos de la misma, para
+  asignarle un valor a cada un de ellas*/
   pitagoras.cateto_adyacente = document.getElementById("inp_catetoa").value;
   pitagoras.cateto_opuesto = document.getElementById("inp_catetoo").value;
 
+  /*invocamos al metodo que posteriormente nos entrega o retorna una respuesta.
+  Dicha se asigna o guarda en variables "respuesta"*/
   let respuesta = pitagoras.calcular_hipotenusa();
 
+  //obtenemos ael <h1> para asignarle y mostrar el resultado de la hipotenusa
   document.getElementById("resultado").textContent = respuesta;
 }
 document.getElementById("btn_calcular").addEventListener("click", ejecutar);
---
+++ repaso_js/fetch_api/main.js
@@ -0,0 +1,9 @@
+function consumir_api()
+{
+
+}
+
+//ejecuto la funcion sin accionarla en un botón
+consumir_api()
+
+//un "api" es una interfaz de programación que sirve para la comunicación html con js; gracias al DOM
