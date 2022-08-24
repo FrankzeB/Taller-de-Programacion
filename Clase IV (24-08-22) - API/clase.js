c0f1f12464edb15411b72d96db70fef1cdcc0275
FrankzeB <franezequielbravo05@gmail.com>
2022-08-24 17:07:15 -0300

    Hicimos una Actividad de Repaso

---
+++ Clase IV (24-08-22) - API/clase.js
@@ -0,0 +1,26 @@
+export default class Criptomonedas {
+
+    constructor(){}
+    
+        obtener_criptos = async () =>{
+        const criptos = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+    
+        const lista_criptos = await criptos.json()
+    
+        let filas = []
+        lista_criptos.forEach((element,index) => {
+            let fila = `
+                <tr>
+                    <td>${index+1}</td>
+                    <td><img style="width:4ron" src="${element.image}"></td>
+                    <td>${element.name}</td>
+                    <td>${element.symbol}</td>
+                    <td>${element.current_price}</td>
+                    <td>${element.total_volume}</td>
+                </tr>
+            `
+            filas.push(fila)
+        });
+        document.getElementById("tbl_body").innerHTML=filas.join("")
+    }
+}
---
+++ Clase IV (24-08-22) - API/main.js
@@ -0,0 +1,4 @@
+import Criptomonedas from "./clase.js";
+
+let criptomonedas = new Criptomonedas();
+criptomonedas.obtener_criptos();
